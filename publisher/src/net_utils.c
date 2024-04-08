3c879eb017bbe23f8f9a89bc2aab91b795bd8523
Kisin Wen <kisinwen@foxmail.com>
2024-04-08 09:08:35 +0800

    fix10

--- publisher/src/net_utils.c
+++ publisher/src/net_utils.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "net_utils.h"
 #include "try.h"
@@ -9,13 +10,13 @@
 int send_string(int connection_fd, char *s) {
     uint32_t len = strlen(s);
     try(send_uint32(connection_fd, len));
-    ssize_t result = send(connection_fd, s, len, NULL);
+    ssize_t result = send(connection_fd, s, len, 0);
     return result > 0 ? 0 : result;
 }
 
 int send_uint32(int connection_fd, uint32_t x) {
     uint32_t conv = htonl(x);
-    ssize_t result = send(connection_fd, &conv, sizeof(conv), NULL);
+    ssize_t result = send(connection_fd, &conv, sizeof(conv), 0);
     return result > 0 ? 0 : result;
 }
 
@@ -28,11 +29,11 @@ int receive_string(int connection_fd, char *s, uint32_t *len) {
 }
 
 int receive_uint32(int connection_fd, uint32_t *x) {
-    ssize_t result = recv(connection_fd, x, sizeof(x), NULL);
+    ssize_t result = recv(connection_fd, x, sizeof(x), 0);
     return result > 0 ? 0 : result;
 }
 
 int receive_byte(int connection_fd, uint8_t *x) {
-    ssize_t result = recv(connection_fd, x, sizeof(x), NULL);
+    ssize_t result = recv(connection_fd, x, sizeof(x), 0);
     return result > 0 ? 0 : result;
 }
